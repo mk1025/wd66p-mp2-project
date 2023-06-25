4e4196e496e716d2845ab176bd9ed5f36ea13b52
Mark Roland Alueta <markintoshroland@gmail.com>
2023-06-26 02:48:08 +0800

    Finished: PHP Server Code

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const express = require("express");
-const phpExpress = require("php-express");
-const mysql = require("mysql");
+const { execSync } = require("child_process");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-// Configuration for PHP to work with Express
-app.set("views", "./php");
-app.engine("php", phpExpress.engine);
-app.set("view engine", "php");
-app.all(/.+\.php$/, (req, res, next) => {
-  phpExpress.router(req, res, next);
+const phpFilesDir = path.join(__dirname, "src", "php");
+const htmlFilesDir = path.join(__dirname, "/src");
+
+app.use("/php", (req, res) => {
+  const url = req.url === "/" ? "/index.php" : req.url;
+  const command = `php -f ${path.join(phpFilesDir, url)}`;
+  const output = execSync(command).toString();
+  res.send(output);
 });
 
-// Create a MySQL connection pool
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "password",
-  //   database: "mydatabase",
-});
+app.use(express.static(htmlFilesDir));
 
-// Starting the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ src/php/index.php
@@ -0,0 +1,4 @@
+<?php
+
+    echo "works <br>";
+    phpinfo();
