633d8c2852a88969f4885d58044c4990269c8c9c
Mark Roland Alueta <markintoshroland@gmail.com>
2023-07-04 19:29:44 +0800

    Updated Server Code

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ const port = 3000;
 
 const phpFilesDir = path.join(__dirname, "src", "php");
 const htmlFilesDir = path.join(__dirname, "src");
+const jsFilesDir = path.join(__dirname, "src", "scripts", "js");
+
+app.use(express.static(htmlFilesDir));
+app.use(express.static(jsFilesDir));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("php/registration", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  const command = `php-cgi -f ${path.join(phpFilesDir, "registration.php")}`;
+  const output = execSync(command, { input: JSON.stringify(data) }).toString();
+  res.send(output);
+});
 
 app.use("/php", (req, res) => {
   const url = req.url === "/" ? "/index.php" : req.url;
@@ -16,7 +31,7 @@ app.use("/php", (req, res) => {
   res.send(output);
 });
 
-app.use(express.static(htmlFilesDir));
+module.exports = app;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
