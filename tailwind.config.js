e675a6bf7d8e47e072c4f15a66db0199d84f5925
Mark Roland Alueta <markintoshroland@gmail.com>
2023-07-15 08:52:33 +0800

    Fix: Updated Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./index.html",
+    "./pages/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
