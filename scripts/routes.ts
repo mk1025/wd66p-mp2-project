5048ffc0529a1cc7194bcaf0222ab832b1367110
Mark Roland Alueta <markintoshroland@gmail.com>
2023-07-20 03:51:30 +0800

    Fix: Forgot to change route paths to the overhaul

--- scripts/routes.ts
+++ scripts/routes.ts
@@ -1,13 +1,17 @@
 export const REGISTRATION_API = "../../api/registration.php";
 export const LOGIN_API = "../../api/login.php";
+export const STUDENTS_API = "../../api/students.php";
 
 export const REGISTRATION_PAGE = "../registration";
 export const REGISTRATION_PAGE_FROM_HOME = "./pages/registration";
 
+export const DASHBOARD_PAGE = "../dashboard";
+export const DASHBOARD_PAGE_FROM_HOME = "./pages/dashboard/";
+
 export const LOGIN_PAGE = "../login";
 export const LOGIN_PAGE_FROM_HOME = "./pages/login/";
 
-export const DASHBOARD_PAGE = "../dashboard";
-export const DASHBOARD_PAGE_FROM_HOME = "./pages/dashboard/";
+export const STUDENTS_PAGE = "../students";
+export const STUDENTS_PAGE_FROM_HOME = "./pages/students/";
 
 export const HOME_PAGE = "../../";
