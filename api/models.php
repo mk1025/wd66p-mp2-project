ae1dedb32a2134cb600535a436de9d54ee4818c0
Mark Roland Alueta <markintoshroland@gmail.com>
2023-07-31 10:21:38 +0800

    Added New Models - Added Model: Transmutations - Added Model: Class Records - Added Model: Components - Added Model: Activities

--- api/models.php
+++ api/models.php
@@ -7,3 +7,7 @@
 define("TBL_USERS", "users");
 define("TBL_SECTIONS", "sections");
 define("TBL_STUDENTS", "students");
+define("TBL_TRANSMUTATIONS", "transmutations");
+define("TBL_RECORDS", "classrecords");
+define("TBL_COMPONENTS", "components");
+define("TBL_ACTIVITIES", "activities");
