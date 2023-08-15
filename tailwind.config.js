235a95ceaf1e6f6becebf0d3b5995a943e810c33
Mark Roland Alueta <markintoshroland@gmail.com>
2023-08-15 19:03:04 +0800

    Additional content in Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./index.html",
     "./pages/**/*.{html,js,ts}",
     "./scripts/**/*.{js,ts}",
+    "./dist/**/*.js",
     "./node_modules/flowbite/**/*.js",
+    "./styles/*.scss",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [require("tailwind-scrollbar"), require("flowbite/plugin")],
 };
