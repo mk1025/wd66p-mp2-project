38923f9bd0376f64d2482c967fb9d4a19fc04ddd
Mark Roland Alueta <markintoshroland@gmail.com>
2023-08-18 20:45:49 +0800

    Added Temporary Class

--- api/functions.php
+++ api/functions.php
@@ -25,3 +25,34 @@ function generateUID($length)
 
     return $sequence;
 }
+
+
+class Functions
+{
+
+    protected function generateUID($length)
+    {
+        $characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $sequence = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomIndex = random_int(0, strlen($characters) - 1);
+            $sequence .= $characters[$randomIndex];
+        }
+
+        return $sequence;
+    }
+
+    protected function createResponse($status, $title, $message, $description, $data = array())
+    {
+        $response = array(
+            "status" => $status,
+            "title" => $title,
+            "message" => $message,
+            "description" => $description,
+            "data" => $data
+        );
+
+        return json_encode($response, JSON_PRETTY_PRINT);
+    }
+}
