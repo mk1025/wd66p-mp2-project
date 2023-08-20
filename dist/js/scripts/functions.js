1164f54499d4d2e3e834f2485269b13dae648136
Mark Roland Alueta <markintoshroland@gmail.com>
2023-08-21 00:44:22 +0800

    Added New Function

--- dist/js/scripts/functions.js
+++ dist/js/scripts/functions.js
@@ -5,3 +5,8 @@ export function DateFormatter(date) {
         year: "numeric",
     }).format(date);
 }
+export function generateElementRandomId() {
+    const prefix = "element";
+    const randomString = Math.random().toString(36).substring(2, 8);
+    return `${prefix}-${randomString}`;
+}
--- scripts/functions.ts
+++ scripts/functions.ts
@@ -1,7 +1,13 @@
 export function DateFormatter(date: Date) {
-  return new Intl.DateTimeFormat("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  }).format(date);
+	return new Intl.DateTimeFormat("en-US", {
+		month: "long",
+		day: "numeric",
+		year: "numeric",
+	}).format(date);
+}
+
+export function generateElementRandomId() {
+	const prefix = "element";
+	const randomString = Math.random().toString(36).substring(2, 8);
+	return `${prefix}-${randomString}`;
 }
