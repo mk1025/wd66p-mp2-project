1b7eb04d99bbcf32faf1b0d9b4537704c5a95428
Mark Roland Alueta <markintoshroland@gmail.com>
2023-08-23 16:57:09 +0800

    Added link to Check Grades Page and Minor Fixes

--- scripts/home.ts
+++ scripts/home.ts
@@ -1,34 +1,28 @@
 import * as Routes from "./routes";
 
-const Register_Button = document.getElementById(
-  "Register_Button",
-) as HTMLButtonElement;
-const Login_Button = document.getElementById(
-  "Login_Button",
-) as HTMLButtonElement;
-const ForStudents_Button = document.getElementById(
-  "ForStudents_Button",
-) as HTMLButtonElement;
+const Register_Button = document.getElementById("Register_Button") as HTMLButtonElement;
+const Login_Button = document.getElementById("Login_Button") as HTMLButtonElement;
+const ForStudents_Button = document.getElementById("ForStudents_Button") as HTMLButtonElement;
 
 const Features_Button = document.getElementById("Features_Button");
 
 Register_Button?.addEventListener("click", () => {
-  window.location.href = Routes.REGISTRATION_PAGE_FROM_HOME;
+	window.location.href = Routes.REGISTRATION_PAGE_FROM_HOME;
 });
 
 Login_Button?.addEventListener("click", () => {
-  window.location.href = Routes.LOGIN_PAGE_FROM_HOME;
+	window.location.href = Routes.LOGIN_PAGE_FROM_HOME;
 });
 
 ForStudents_Button?.addEventListener("click", () => {
-  window.location.href = "#";
+	window.location.href = "./pages/checkgrades";
 });
 
 Features_Button?.addEventListener("click", () => {
-  let element = document.getElementById("Features_Section") as HTMLElement;
+	let element = document.getElementById("Features_Section") as HTMLElement;
 
-  element.scrollIntoView({
-    block: "start", // Scroll the element to the top of the viewport
-    behavior: "smooth", // Scroll with smooth animation
-  });
+	element.scrollIntoView({
+		block: "start", // Scroll the element to the top of the viewport
+		behavior: "smooth", // Scroll with smooth animation
+	});
 });
