17e1ae70636ac97f42ef06dd9686bd52c807ffad
Mark Roland Alueta <markintoshroland@gmail.com>
2023-08-23 17:00:09 +0800

    Temporarily updated Env Script

--- scripts/env.ts
+++ scripts/env.ts
@@ -3,4 +3,4 @@ export const PROFILE_MIN_HEIGHT: number = 200;
 export const PROFILE_MAX_WIDTH: number = 2048;
 export const PROFILE_MAX_HEIGHT: number = 2048;
 
-export const CONSOLE_LOG: boolean = true;
+export const CONSOLE_LOG: boolean = false;
